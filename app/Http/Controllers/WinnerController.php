0b4b23b9fc9472120eb7cec2f83caa2f6499b4e6
Jorge Eduardo <corpjorge@hotmail.com>
2021-11-17 08:26:10 -0500

    Update WinnerController.php

--- app/Http/Controllers/WinnerController.php
+++ app/Http/Controllers/WinnerController.php
@@ -22,7 +22,7 @@ class WinnerController extends Controller
     public function winners()
     {
         if (auth()->user()->id === 1){
-            return Winner::all()->load('user');
+            return Winner::orderBy('id', 'desc')->limit(20)->get()->load('user');
         }
 
         abort(404);
